e9252d66b03efb87c67774e4aba6cbdb3c00453a
malahat456 <120156684+malahat456@users.noreply.github.com>
2022-12-18 08:29:02 -0800

    Add files via upload

---
+++ task8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+system("color 65");
+cout<<"   .....           eeeee     eeeee         .................   "<<endl;
+cout<<"   ......         e     e   e     e        ................    "<<endl;
+cout<<"   .......           eee    e     e        ...............     "<<endl;
+cout<<"   ........        ee       e     e        ..............      "<<endl;
+cout<<"    ........      eeeeeee    eeeee  th     .............       "<<endl;
+cout<<"       ......     ....................     ............        "<<endl;
+cout<<"         .....       C E N T U R Y         ...........         "<<endl;
+cout<<"          .....   ....................     ..........          "<<endl;
+cout<<"           .....  eeeee eeeee e   e eeeee  ........            "<<endl;
+cout<<"               ..   e   e      e e    e    ..                  "<<endl;
+cout<<"              _||_  e   eee     e     e   _||_                 "<<endl;
+cout<<"             |    | e   e      e e    e  |    |                "<<endl;
+cout<<"        _____|____|_e   eeeee e   e   e__|____|______          "<<endl;
+}
